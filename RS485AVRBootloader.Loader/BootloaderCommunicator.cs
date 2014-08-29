e644fe41d9b7c92fbea18ae569a1d32e1c6b4a9b
Mateusz Świetlicki <mateusz@swietlicki.net>
2014-08-29 13:38:07 +0200

    Fixing end of program stream

--- RS485AVRBootloader.Loader/BootloaderCommunicator.cs
+++ RS485AVRBootloader.Loader/BootloaderCommunicator.cs
@@ -38,16 +38,38 @@ namespace SerialAVRBootloader.Loader
 
             _communicator.Write("uw");
 
-            while (dataStream.Position < dataStream.Length)
+            bool endofFile = false;
+            while (!endofFile && dataStream.Position < dataStream.Length)
             {
-                _communicator.Write(new byte[] {1}, 0, 1);
+                _communicator.Write(new byte[] { 1 }, 0, 1); //Send start 1
                 var data = new byte[64];
-                dataStream.Read(data, 0, 64);
+
+
+                if (dataStream.Position + 64 >= dataStream.Length)
+                {
+                    var toEnd = (int) (dataStream.Length - dataStream.Position);
+                    dataStream.Read(data, 0, toEnd);
+
+                    if (dataStream.CanSeek)
+                    {
+                        dataStream.Seek(0, SeekOrigin.Begin);
+                        dataStream.Read(data, toEnd, 64 - toEnd);
+                    }
+                    endofFile = true;
+                }
+                else
+                {
+                    dataStream.Read(data, 0, 64);
+                }
+
+
                 _communicator.Write(data, 0, 64);
 
-                if (_communicator.ReadChar() != '@')
-                    throw new CommunicationLostExpection();
+                if (!endofFile)
+                    if (_communicator.ReadChar() != '@')
+                        throw new CommunicationLostExpection();
             }
+            _communicator.Write(new byte[] { 0 }, 0, 1); //Send end 0
         }
     }
 }
